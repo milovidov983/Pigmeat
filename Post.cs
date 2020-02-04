658f596fdfa97e42e29fd5817e50cd97cd0764cd
Emil Sayahi <emilsayahi@foresthills.edu>
2020-02-04 12:01:50 -0500

    Implemented generating post list JObject (part of #8)
    This is to create a JObject which will then be given its own context ("site.collections.<string>"), so pagination will become possible by iterating through this list/array of posts.

--- Post.cs
+++ Post.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using Newtonsoft.Json.Linq;
 
@@ -13,6 +14,24 @@ namespace WDHAN
         public Post()
         {
             
+        }
+        public static JObject getPosts(string collectionName, Config siteConfig) 
+        {
+            List<string> postList = new List<string>();
+            foreach(var collection in siteConfig.collections)
+            {
+                foreach(var key in collection.Keys)
+                {
+                    if(key.Equals(collectionName, StringComparison.OrdinalIgnoreCase))
+                    {
+                        foreach(var post in Directory.GetFiles(siteConfig.collections_dir + "/_" + key))
+                        {
+                            postList.Add(File.ReadAllText(post));
+                        }
+                    }
+                }
+            }
+            return JObject.FromObject(postList);
         }
     }
 }
