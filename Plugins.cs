9194797f41c3291aa67dee171fa385294699ba3f
Emil Sayahi <em@emsa.cf>
2020-04-05 20:46:47 -0400

    Resolved problem with loading plugins

--- Plugins.cs
+++ Plugins.cs
@@ -13,24 +13,24 @@ namespace WDHAN
         public static void getPlugins(string[] args)
         {
             var siteConfig = GlobalConfiguration.getConfiguration();
-            if(args.Length == 1)
+            try
             {
-                foreach(var file in Directory.GetFiles(siteConfig.source + "/" + siteConfig.plugins_dir, "*", SearchOption.AllDirectories))
+                if(args.Length == 1 || !args[args.Length - 2].Equals("-f", StringComparison.OrdinalIgnoreCase))
                 {
-                    Console.WriteLine("Found plugins directory, " + siteConfig.source + "/" + siteConfig.plugins_dir);
-                    if(siteConfig.plugins.Contains(Path.GetFileNameWithoutExtension(file)))
+                    foreach(var file in Directory.GetFiles(siteConfig.source + "/" + siteConfig.plugins_dir, "*", SearchOption.AllDirectories))
                     {
-                        Console.WriteLine("Loading plugin: " + Path.GetFileName(file));
-                        dynamic script = CSScript.Evaluator.LoadCode(File.ReadAllText(file));
-                        script.Main(args);
+                        Console.WriteLine("Found plugins directory, " + siteConfig.source + "/" + siteConfig.plugins_dir);
+                        if(siteConfig.plugins.Contains(Path.GetFileNameWithoutExtension(file)))
+                        {
+                            Console.WriteLine("Loading plugin: " + Path.GetFileName(file));
+                            dynamic script = CSScript.Evaluator.LoadCode(File.ReadAllText(file));
+                            script.Main(args);
+                        }
                     }
                 }
-            }
-            else
-            {
-                var pluginsDirectory = siteConfig.source + "/" + siteConfig.plugins_dir;
-                try
+                else
                 {
+                    var pluginsDirectory = siteConfig.source + "/" + siteConfig.plugins_dir;
                     if(args[2].Equals("-f", StringComparison.OrdinalIgnoreCase))
                     {
                         Console.WriteLine("Loading plugin: " + args[1]);
@@ -38,14 +38,15 @@ namespace WDHAN
                         script.Main(args);
                     }
                 }
-                catch(IndexOutOfRangeException)
+            }
+            catch(IndexOutOfRangeException)
+            {
+                var pluginsDirectory = siteConfig.source + "/" + siteConfig.plugins_dir;
+                if(siteConfig.plugins.Contains(args[1]))
                 {
-                    if(siteConfig.plugins.Contains(args[1]))
-                    {
-                        Console.WriteLine("Loading plugin: " + args[1]);
-                        dynamic script = CSScript.Evaluator.LoadCode(File.ReadAllText(pluginsDirectory + "/" + args[1]));
-                        script.Main(args);
-                    }
+                    Console.WriteLine("Loading plugin: " + args[1]);
+                    dynamic script = CSScript.Evaluator.LoadCode(File.ReadAllText(pluginsDirectory + "/" + args[1]));
+                    script.Main(args);
                 }
             }
         }
