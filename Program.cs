a8bf443c5324573d6af45745e691980c88a11855
Emil Sayahi <em@emsa.cf>
2020-06-03 06:17:24 -0400

    Added `{{ global.include-raw }}` and `{{ global.include-html }}`

--- Program.cs
+++ Program.cs
@@ -84,6 +84,8 @@ namespace Pigmeat
         {
             JObject Global = JObject.Parse(IO.GetGlobal());
             string[] IncludedFiles = JsonConvert.DeserializeObject<string[]>(Global["include"].ToString());
+            string[] IncludedFilesHTML = JsonConvert.DeserializeObject<string[]>(Global["include-html"].ToString());
+            string[] IncludedFilesRaw = JsonConvert.DeserializeObject<string[]>(Global["include-raw"].ToString());
             for(int i = 0; i < 2; i++)
             {
                 foreach(var directory in Directory.GetDirectories("./", "_*", SearchOption.TopDirectoryOnly))
@@ -108,15 +110,6 @@ namespace Pigmeat
                                 Console.WriteLine(file + " → " + "./output/" + Path.GetDirectoryName(file) + "/" + Path.GetFileNameWithoutExtension(file) + ".css");
                             }
                         }
-                        else if(File.ReadAllText(file).Contains("---"))
-                        {
-                            JObject PageObject = Page.GetPageObject(file, false);
-                            IO.RenderPage(PageObject, directory.Substring(3), file, false);
-                            if(i == 1)
-                            {
-                                Console.WriteLine(file + " → " + "./output/" + PageObject["url"].ToString());
-                            }
-                        }
                         else if(!Path.GetExtension(file).Equals(".json") && !Path.GetExtension(file).Equals(".yml"))
                         {
                             Directory.CreateDirectory("./output/" + Path.GetDirectoryName(file));
@@ -128,13 +121,61 @@ namespace Pigmeat
                         }
                     }
                 }
-                foreach(var file in IncludedFiles)
+                try
                 {
-                    JObject PageObject = Page.GetPageObject(file, true);
-                    IO.RenderPage(PageObject, "", file, true);
+                    foreach(var file in IncludedFiles)
+                    {
+                        Directory.CreateDirectory("./output/" + Path.GetDirectoryName(file));
+                        File.Copy(file, "./output/" + file, true);
+                        if(i == 1)
+                        {
+                            Console.WriteLine(file + " → " + "./output/" + file);
+                        }
+                    }
+                }
+                catch(Exception e)
+                {
                     if(i == 1)
                     {
-                        Console.WriteLine(file + " → " + "./output/" + PageObject["url"].ToString());
+                        Console.WriteLine(e);
+                    }
+                }
+                try
+                {
+                    foreach(var file in IncludedFilesHTML)
+                    {
+                        JObject PageObject = Page.GetPageObject(file, true);
+                        IO.RenderPage(PageObject, "", file, true);
+                        if(i == 1)
+                        {
+                            Console.WriteLine(file + " → " + "./output/" + PageObject["url"].ToString());
+                        }
+                    }
+                }
+                catch(Exception e)
+                {
+                    if(i == 1)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+                try
+                {
+                    foreach(var file in IncludedFilesRaw)
+                    {
+                        JObject PageObject = Page.GetPageObject(file, false);
+                        IO.RenderPage(PageObject, "", file, false);
+                        if(i == 1)
+                        {
+                            Console.WriteLine(file + " → " + "./output/" + PageObject["url"].ToString());
+                        }
+                    }
+                }
+                catch(Exception e)
+                {
+                    if(i == 1)
+                    {
+                        Console.WriteLine(e);
                     }
                 }
                 IO.CleanCollections();
@@ -157,7 +198,7 @@ namespace Pigmeat
             File.WriteAllText("./_posts/README", "This is where your posts should go.");
             File.WriteAllText("./_pages/README", "This is where your pages should go.");
             File.WriteAllText("./_files/README", "This is where your loose files (data, media, stylesheets, etc.) should go.");
-            File.WriteAllText("./_global.yml", "title: Pigmeat Project\nculture: \"en-US\"");
+            File.WriteAllText("./_global.yml", "title: Pigmeat Project\nculture: \"en-US\"\ninclude:\ninclude-raw:\ninclude-html:");
             File.WriteAllText("./drafts/README", "This is where your Markdown and HTML documents should go if you don't want them to be published.");
             File.WriteAllText("./layouts/README", "This is where your HTML page templates go.");
             File.WriteAllText("./includes/README", "This is where your HTML snippets go.");
