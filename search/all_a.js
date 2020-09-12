bf0397b1993e9d8d9efe9dc53b58edc1f656a5b8
Doxygen <em@emsa.cf>
2020-09-12 08:55:34 +0000

    Updating documentation 1cf27221710f71b9d5ed28797ddb7c8eb3577ac2

--- search/all_a.js
+++ search/all_a.js
@@ -3,7 +3,6 @@ var searchData=
   ['i_5fday',['i_day',['../class_pigmeat_1_1_core_1_1_page.html#a641dbf01179f259019eadab117d677df',1,'Pigmeat::Core::Page']]],
   ['i_5fmonth',['i_month',['../class_pigmeat_1_1_core_1_1_page.html#a5acea52b07dee7e393f8b2432137d0e0',1,'Pigmeat::Core::Page']]],
   ['includedirectory',['IncludeDirectory',['../class_pigmeat_1_1_core_1_1_i_o.html#ab8a3a50a1c1ae3b1183e298846b77abd',1,'Pigmeat::Core::IO']]],
-  ['install',['Install',['../class_pigmeat_1_1_program.html#a48e58e60d726bd462ea0d01e075fc3f1',1,'Pigmeat::Program']]],
   ['io',['IO',['../class_pigmeat_1_1_core_1_1_i_o.html',1,'Pigmeat::Core']]],
   ['io_2ecs',['IO.cs',['../_i_o_8cs.html',1,'']]]
 ];
--- search/functions_7.js
+++ search/functions_7.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['includedirectory',['IncludeDirectory',['../class_pigmeat_1_1_core_1_1_i_o.html#ab8a3a50a1c1ae3b1183e298846b77abd',1,'Pigmeat::Core::IO']]],
-  ['install',['Install',['../class_pigmeat_1_1_program.html#a48e58e60d726bd462ea0d01e075fc3f1',1,'Pigmeat::Program']]]
+  ['includedirectory',['IncludeDirectory',['../class_pigmeat_1_1_core_1_1_i_o.html#ab8a3a50a1c1ae3b1183e298846b77abd',1,'Pigmeat::Core::IO']]]
 ];
