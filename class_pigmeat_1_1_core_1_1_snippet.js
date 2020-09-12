accf1327e3de8320af3328573ac820a4a22899d5
Doxygen <em@emsa.cf>
2020-09-12 08:50:14 +0000

    Updating documentation 6f50b30367ceb746e0440601107799a1622240a5

--- class_pigmeat_1_1_core_1_1_snippet.js
+++ class_pigmeat_1_1_core_1_1_snippet.js
@@ -1,7 +1,9 @@
 var class_pigmeat_1_1_core_1_1_snippet =
 [
+    [ "GetArguments", "class_pigmeat_1_1_core_1_1_snippet.html#a359cb5b64ee741ebf8cde64db13afec4", null ],
+    [ "GetKeys", "class_pigmeat_1_1_core_1_1_snippet.html#a587d72ee7d694c9a05b9af340e172d3c", null ],
+    [ "GetValues", "class_pigmeat_1_1_core_1_1_snippet.html#abdee9a9d3a2ed0497158bcfb5461c60a", null ],
     [ "Parse", "class_pigmeat_1_1_core_1_1_snippet.html#a54ea5e1d3a8cd6308a1b1c9f01457146", null ],
     [ "Render", "class_pigmeat_1_1_core_1_1_snippet.html#ac3dd182d3699b9997372e1baa768de2b", null ],
-    [ "Input", "class_pigmeat_1_1_core_1_1_snippet.html#aee52278a8fcfccb9c2fa86450c2ff5d6", null ],
     [ "Variables", "class_pigmeat_1_1_core_1_1_snippet.html#a66010995ab03ad7e67083a5c54998b87", null ]
 ];
